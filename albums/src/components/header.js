a1902c575bafb7f055fe3d314f40fa965832b048
Yuting Chou <ytingchou@gmail.com>
2016-11-07 23:09:56 +1100

    Added style for header.

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,10 +1,25 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 // Make the component available to other parts of the app
