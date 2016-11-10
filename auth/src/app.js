3bb1df6a21247c7512f8b10a790bd5d4663222ec
Yuting Chou <ytingchou@gmail.com>
2016-11-11 00:49:01 +1100

    Added firebase support

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  ComponentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD8jW4W64Yq_lPVsfJ-b6DrRqt8BlyYjXw",
+      authDomain: "authentication-5347b.firebaseapp.com",
+      databaseURL: "https://authentication-5347b.firebaseio.com",
+      storageBucket: "authentication-5347b.appspot.com",
+      messagingSenderId: "826901678672"
+    });
+  }
+
   render() {
     return (
       <View>
