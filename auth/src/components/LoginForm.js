705414fa932d663c2c323f954ef5f990cd2becca
Yuting Chou <ytingchou@gmail.com>
2016-11-11 19:29:38 +1100

    Added password Input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
@@ -17,6 +17,13 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
         </CardSection>
 
         <CardSection>
