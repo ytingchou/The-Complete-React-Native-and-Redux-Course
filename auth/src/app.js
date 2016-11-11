5fcecff5d29463c2c9a03bed07c3097bd1db4c93
Yuting Chou <ytingchou@gmail.com>
2016-11-11 23:50:54 +1100

    More rendering for authentication state

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -26,11 +26,32 @@ class App extends Component {
     });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
