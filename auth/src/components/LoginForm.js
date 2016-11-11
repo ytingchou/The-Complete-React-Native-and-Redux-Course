d5569d47afeb10ecac28686b269115a5b81f031c
Yuting Chou <ytingchou@gmail.com>
2016-11-11 19:01:32 +1100

    Added Input component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { text: '' };
+
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <Input
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
 
         <CardSection>
           <Button>
