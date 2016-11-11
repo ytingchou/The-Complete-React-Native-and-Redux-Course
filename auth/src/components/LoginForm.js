488aacc389b1dd7926f2a9656982d5cb5940a6d4
Yuting Chou <ytingchou@gmail.com>
2016-11-11 19:22:07 +1100

    Stored email into state

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
 
   render() {
     return (
@@ -11,8 +11,8 @@ class LoginForm extends Component {
           <Input
             placeholder="user@email.com"
             label="Email"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
