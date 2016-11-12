490307b4aa8f897c25eaf99731f8950b7aae59c7
Yuting Chou <ytingchou@gmail.com>
2016-11-12 21:23:28 +1100

    Added actions.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+    console.log(this.props);
 
     return (
       <CardSection>
@@ -23,4 +26,4 @@ const styles = {
   }
 };
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
