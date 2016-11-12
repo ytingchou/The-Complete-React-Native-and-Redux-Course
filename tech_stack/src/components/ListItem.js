c579dec97b32cf39bc491f30b6ef2f2897069a30
Yuting Chou <ytingchou@gmail.com>
2016-11-12 22:36:03 +1100

    Added spring animation.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import {
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
 
     if (expanded) {
       return (
-        <Text>{library.description}</Text>
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
       );
     }
   }
