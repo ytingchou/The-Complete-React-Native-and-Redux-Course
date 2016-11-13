85a21475463fffb9592d33a9ab13be0889a6c214
Yuting Chou <ytingchou@gmail.com>
2016-11-14 00:49:59 +1100

    Used Redux Thunk to handle "login user"

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -2,6 +2,7 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
 } from './types';
+import firebase from 'firebase';
 
 export const emailChanged = (text) => {
   return {
@@ -15,4 +16,13 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+      });
+  }
 };
