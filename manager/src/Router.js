941120df7a52c6173a1c3635ab664b56ae89db04
Yuting Chou <ytingchou@gmail.com>
2016-11-15 23:58:10 +1100

    Added a new scene to display EmployeeList

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginFrom from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key="login" component={LoginFrom} title="Please Login" />
+      <Scene key="employeeList" component={EmployeeList} title="Employees" />
     </Router>
   );
 };
