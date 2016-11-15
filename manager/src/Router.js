d9330dca626af38b707bec50454f6886df5e46b1
Yuting Chou <ytingchou@gmail.com>
2016-11-16 00:58:23 +1100

    Grouped scenes

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -6,8 +6,16 @@ import EmployeeList from './components/EmployeeList';
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="login" component={LoginFrom} title="Please Login" />
-      <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      <Scene key="auth">
+        <Scene key="login" component={LoginFrom} title="Please Login" />
+      </Scene>
+
+      <Scene key="main">
+        <Scene
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees" />
+      </Scene>
     </Router>
   );
 };
